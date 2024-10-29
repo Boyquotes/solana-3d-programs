be63292a1c7ce869607caafcfd12b424d03a3bdd
Boyquotes <webmaster@linux-live-cd.org>
2024-10-29 09:44:49 +0100

    wallet connection and call program(don't work, try umi?)

--- example/SonarWatch.tsx
+++ example/SonarWatch.tsx
@@ -8,10 +8,61 @@ import * as React from 'react';
 import { useRef, useMemo, useState, useEffect, Suspense } from "react";
 import ShotCube from "./ShotCube";
 import { Connection, PublicKey, clusterApiUrl, Transaction, SystemProgram, Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import * as solanaWeb3 from '@solana/web3.js';
 import BoxWithTexture from './BoxWithTexture';
+
+import { GlowWalletAdapter } from '@solana/wallet-adapter-glow';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { registerWalletAdapter, SOLANA_MAINNET_CHAIN } from '@solana/wallet-standard';
+import { useWallets } from '@wallet-standard/react';
+import { getWallets } from '@wallet-standard/app';
+
+// import { BN, Idl, IdlAccounts, Program } from "@coral-xyz/anchor";
+// import { BN, Program } from "@coral-xyz/anchor";
+import BN from 'bn.js';
+import { AnchorProvider, Program, web3 } from "@coral-xyz/anchor";
+import idl from "./idl.json";
+
+const network = "https://api.devnet.solana.com"; // Use the appropriate network
+
+const opts = {
+    preflightCommitment: "processed",
+  };
+
 
 export default function SonarWatch() {
 
+
+    // Program ID from the IDL metadata
+    const [program, setProgram] = useState(null);
+    // const programId = new PublicKey(idl.metadata.address);
+
+    useEffect(() => {
+        const loadProgram = async () => {
+          const connection = new Connection(network, opts.preflightCommitment);
+          
+          // Assuming Phantom wallet extension
+          const provider = new AnchorProvider(
+            connection,
+            window.solana, // Wallet instance
+            opts
+          );
+    
+          // Program ID from the IDL metadata
+          const programId = new PublicKey('CAjif8p66ekjjRxwGeZxEUGr19Q34cCUSv8ShDFH1cYe');
+          console.log("programId "+programId);
+          
+          // Load the program using the IDL
+        //   const loadedProgram = new Program(idl, programId, provider);
+        //   console.log("loadedProgram "+loadedProgram);
+        //   setProgram(loadedProgram);
+        };
+    
+        loadProgram();
+      }, []);
+
+
+    // console.log(programId);
     const [loading, setLoading] = useState(false);
     const [reload, setReload] = useState(false);
     const [nbToken, setNbToken] = useState(0);
@@ -32,9 +83,15 @@ export default function SonarWatch() {
     //   setAddressWalletSolana("GthTyfd3EV9Y8wN6zhZeES5PgT2jQVzLrZizfZquAY5S");
     const [balance, setBalance] = useState(null);
 
+    const [walletsAvailable, setWalletsAvailable] = useState<string[]>([]);
+
     const provider = window.solana;
-    // console.log("provider");
-    // console.log(provider);
+    console.log("provider solana");
+    console.log(provider);
+    console.log(solanaWeb3);
+    console.log(window);
+
+
     // const connectWallet = async () => {
         // try {
         //     if (provider && provider.isPhantom) {
@@ -81,37 +138,39 @@ export default function SonarWatch() {
             if(!address && !addressWalletSolana){
                 setAddressWalletSolana("GthTyfd3EV9Y8wN6zhZeES5PgT2jQVzLrZizfZquAY5S");
             }
-            console.log(addressWalletSolana+" addressWalletSolana");
-            const apiUrl = "https://portfolio-api.sonar.watch/v1/portfolio/fetch?useCache=false&address=" + addressWalletSolana + "&addressSystem=solana"
-            try {
-                const response = await fetch(apiUrl, {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': `Bearer alyraFqxeL6luVs9a3XQ8KG7n2H`,
-                        'Content-Type': 'application/json'
+            if(addressWalletSolana != undefined){
+                console.log(addressWalletSolana+" addressWalletSolana");
+                const apiUrl = "https://portfolio-api.sonar.watch/v1/portfolio/fetch?useCache=false&address=" + addressWalletSolana + "&addressSystem=solana"
+                try {
+                    const response = await fetch(apiUrl, {
+                        method: 'GET',
+                        headers: {
+                            'Authorization': `Bearer alyraFqxeL6luVs9a3XQ8KG7n2H`,
+                            'Content-Type': 'application/json'
+                        }
+                    });
+    
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
                     }
-                });
-
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
+                    const data = await response.json();
+                    // console.log("data sonar");
+                    // console.log(data);
+                    // console.log(data.fetcherReports);
+                    // console.log(data.owner);
+                    // console.log(data.tokenInfo);
+                    if(data.tokenInfo.length == 0){
+                        console.log("empty wallet");
+                    }
+                    // setDataSonarWatch(data);
+                    setNbToken(0);
+                    displayDataSonar(data);
+                    setLoading(false);
+                    setReload(false);
+                } catch (error) {
+                    setLoading(false);
+                    console.error('Error fetching data:', error);
                 }
-                const data = await response.json();
-                // console.log("data sonar");
-                // console.log(data);
-                // console.log(data.fetcherReports);
-                // console.log(data.owner);
-                // console.log(data.tokenInfo);
-                if(data.tokenInfo.length == 0){
-                    console.log("empty wallet");
-                }
-                // setDataSonarWatch(data);
-                setNbToken(0);
-                displayDataSonar(data);
-                setLoading(false);
-                setReload(false);
-            } catch (error) {
-                setLoading(false);
-                console.error('Error fetching data:', error);
             }
         };
         fetchDataSonar();
@@ -219,22 +278,106 @@ export default function SonarWatch() {
             });
     };
 
+
+    useEffect(() => {
+        const getWalletSolana = async () => {
+            // const walletsStd = useWallets();
+            const walletsStd = getWallets();
+            console.log("walletstd "+walletsStd);
+            console.log("walletstd "+JSON.stringify(walletsStd, null, 2));
+            // return (
+            //     <ul>
+            //         {walletsStd.map((wallet, index) => (
+            //             <li key={index}>{wallet.name}</li>
+            //         ))}
+            //     </ul>
+            // );
+
+            // {walletsStd.entries(user).map(([key, value]) => (
+            //     <li key={key}>
+            //       <strong>{key}:</strong> {value}
+            //     </li>
+            //   ))}
+
+
+            // {walletsStd.map((wallet, index) => (
+            //     console.log("walstd "+wallet.name)
+            // ))}
+        }
+        getWalletSolana();
+    }, []);
     // useEffect(() => {
     //     console.log("data reload")
     //     displayDataSonar();
     // }, [dataSonarWatch]);
 
     useEffect(() => {
+        console.log("laaaa");
+        if (window.solflare) {
+            console.log("walletttsf "+walletsAvailable);
+            console.log("Le wallet Solflare est installé.");
+            setWalletsAvailable((walletsAvailable) => [...walletsAvailable, "Solflare"]);
+        } else {
+            console.log("Le wallet Solflare n'est pas installé.");
+        }
+        if (window.xnft) {
+            console.log("walletttsbb "+walletsAvailable);
+            console.log("Le wallet Backpack est installé.");
+            setWalletsAvailable((walletsAvailable) => [...walletsAvailable, "Backpack"]);
+            console.log("walletttsbb2  "+walletsAvailable);
+        } else {
+            console.log("Le wallet Backpack n'est pas installé.");
+        }
+
+        if (provider && provider.isPhantom) {
+            console.log("Le wallet Phantom est installé.");
+            setWalletsAvailable((walletsAvailable) => [...walletsAvailable, "Phantom"]);
+        } else {
+            console.log("Le wallet Phantom n'est pas installé.");
+        }
+        // let test= "tere";
+        // setWalletsAvailable([...walletsAvailable, "terre"]);
+    }, [window]);
+    console.log("wallettts "+walletsAvailable);
+    console.log(walletsAvailable);
+
+    const addValueToArray = (value) => {
+        setNewTextWallet((prevValues) => [...prevValues, value]);
+        setIsUpdated(true);
+      };
+
+    const [isUpdated, setIsUpdated] = useState(false);
+    useEffect(() => {
+        if (isUpdated) {
+          console.log('Tableau mis à jour:', walletsAvailable);
+          // Exécuter une autre opération après la mise à jour
+          // Par exemple, ajouter une autre valeur
+        //   addAnotherValueAfterUpdate();
+          setIsUpdated(false);
+        }
+      }, [walletsAvailable, isUpdated]);
+
+
+    useEffect(() => {
         const getWalletSolana = async () => {
             let newTextWallet;
+            let wallets=String[''];
             try {
-                if (provider && provider.isPhantom) {
+                console.log("provider wallettts");
+                console.log(provider);
+                console.log("wallettts "+walletsAvailable.length);
+                console.log(walletsAvailable.length);
+                if (walletsAvailable.length > 0) {
                     // Connect to the wallet
-                    const response = await provider.connect();
-                    const publicKey = response.publicKey.toString();
-                    console.log("publicKey");
-                    console.log(publicKey);
-                    setAddressWalletSolana(publicKey);
+                    // const response = await provider.connect();
+                    // const publicKey = response.publicKey.toString();
+                    console.log("publicKey wallettts");
+                    // console.log(publicKey);
+                    // setAddressWalletSolana(publicKey);
+
+                    const responseBackpackConnect = await window.backpack.connect();
+                    // const responseSolflareConnect = await window.solflare.connect();
+
                     // Fetch the balance
                     // Convert Base58 string to PublicKey object
                     // const publicKeyForBalance = new PublicKey(publicKey)
@@ -244,26 +387,51 @@ export default function SonarWatch() {
                     // // Convert the balance from lamports to SOL (1 SOL = 1,000,000,000 lamports)
                     // const balanceInSOL = balance / LAMPORTS_PER_SOL;
                     // console.log(`Balance: ${balanceInSOL} SOL`);
-                    newTextWallet = (<Text
-                        rotation={[0, Math.PI, 0]}
-                        position={[0, 4, 0]}
-                        color="black"
-                        fontSize={0.5}
-                        onClick={checkYourAddress}
-                        >View your portfolio
-                        </Text>);
+                    let y = 4;
+                    walletsAvailable.map((item, i) => {
+                        console.log("item "+item)
+                        newTextWallet = (<Text
+                            rotation={[0, Math.PI, 0]}
+                            position={[0, y, 0]}
+                            color="black"
+                            fontSize={0.5}
+                            >View your {item} portfolio
+                            </Text>);
+                            y += 4;
+                            addValueToArray(newTextWallet);
+                    //     setNewTextWallet((walletsAvailable) => [...walletsAvailable, newTextWallet]);
+                    })
+                    
+                    // newTextWallet = (<Text
+                    //     rotation={[0, Math.PI, 0]}
+                    //     position={[0, y, 0]}
+                    //     color="black"
+                    //     fontSize={0.5}
+                    //     >View your back portfolio
+                    //     </Text>)
+                    // addValueToArray(newTextWallet);
+                    // // setNewTextWallet((walletsAvailable) => [...walletsAvailable, newTextWallet]);
+                    // newTextWallet = (<Text
+                    //     rotation={[0, Math.PI, 0]}
+                    //     position={[0, y+2, 0]}
+                    //     color="black"
+                    //     fontSize={0.5}
+                    //     >View your sol portfolio
+                    //     </Text>)
+                    // addValueToArray(newTextWallet);
+                    // setNewTextWallet((walletsAvailable) => [...walletsAvailable, newTextWallet]);
                 }
-                else{
-                    newTextWallet = (<Text
-                        rotation={[0, Math.PI, 0]}
-                        position={[0, 4, 0]}
-                        color="black"
-                        fontSize={0.5}
-                        onClick={checkYourAddress}
-                        >You don't have a phantom wallet!
-                        </Text>);
-                }
-                setNewTextWallet(newTextWallet);
+                // else{
+                //     newTextWallet = (<Text
+                //         rotation={[0, Math.PI, 0]}
+                //         position={[0, 4, 0]}
+                //         color="black"
+                //         fontSize={0.5}
+                //         onClick={checkYourAddress}
+                //         >You don't have a wallet!
+                //         </Text>);
+                //         setNewTextWallet((prevMeshes) => [...prevMeshes, newTextWallet]);
+                // }
                 setWatchMe(false);
                 setLoading(false);
             } catch (err) {
@@ -279,7 +447,7 @@ export default function SonarWatch() {
         // });
         getWalletSolana();
         // fetchDataSonar();
-    }, [watchMe]);
+    }, [walletsAvailable]);
 
 
     // audioLoader.load( 'gling_coin.wav', function( buffer ) {
@@ -302,7 +470,7 @@ export default function SonarWatch() {
             console.log(publicKey);
             setAddressWalletSolana(publicKey);
         }
-        // setWatchMe(true);
+        setWatchMe(true);
         setLoading(true);
         setReload(true);
         // fetchDataSonar();
@@ -372,12 +540,19 @@ else{
 
 console.log("length wallet "+lengthWallet);
 calculMiddleWallet = lengthWallet / 2;
-console.log("middleWallet wallet "+calculMiddleWallet);
+console.log("middleWallet wallet "+newTextWallet);
 // setMiddleWallet(calculMiddleWallet);
  return (
         <>
             <group position={[0, 0, 10]}>
-                {newTextWallet}
+                {newTextWallet.map((item, i, arr) => {
+                    console.log("item "+JSON.stringify(item, null, 2));
+                        return (
+                            <>
+                                {item}
+                            </>
+                    )})
+                }
                 <Text
                 rotation={[0, Math.PI, 0]}
                 position={[-11, 5, 0]}
